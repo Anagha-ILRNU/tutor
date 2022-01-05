53c760ea89e7c4efb83389fba2b507507c0e6858
alanhsegoy <59256980+alanhsegoy@users.noreply.github.com>
2022-01-05 23:46:44 +0530

    Updated Locator

--- testCases/test_TutoringCreateSession.py
+++ testCases/test_TutoringCreateSession.py
@@ -1,14 +1,17 @@
 from pageObjects.DashboardPage import DashboardPage
 from pageObjects.TutorLoginPage import LoginPage
-
+from utilities.readproperties import ReadConfig
 
 class Opensession:
-    loginURL = "https://uat.ilrnu.com/login/"
-    email = "yatutor200@gmail.com"
-    password = "India@2020"
+    loginURL = ReadConfig.getApplicationURL()
+    email = ReadConfig.getemail()
+    password = ReadConfig.getpassword()
+    path = "testdata/Tutor addsubject.xlsx"
+
+    def __init__(self):
+        self.driver = setup
 
     def test_createOpenSession(self, setup):
-        self.driver = setup
         self.driver.get(self.loginURL)
         self.lp = LoginPage(self.driver)
         self.lp.setEmail(self.email)
